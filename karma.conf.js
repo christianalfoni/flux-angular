9ecc667224562bc7f1ee26113698137d5ccf1599
Jason Rust <rustyparts@gmail.com>
2015-12-07 17:23:42 -0800

    Set compatibility up to angular 1.4
    Also moved angular-mocks dependency to bower so its not hardcoded.

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'bower_components/angular/angular.js',
-      'tests/angular-mocks.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       'build/flux-angular.js',
       'tests/*-spec.js'
     ],
